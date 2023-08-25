585ad4b64589bf6656d26ec28ca818552a8619e5
andbudha <andreibartov@gmail.com>
2023-08-25 20:46:32 +0200

    getting total cost  func-ty added

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,6 +1,14 @@
+import { useDispatch, useSelector } from "react-redux";
 
 export const CarValue = () => {
+    const dispatch = useDispatch();
+    const carList = useSelector(state => state.cars.carList);
+    const carBrand = useSelector(state => state.cars.searchTerm);
+    const filteredCarList = carList.filter(car => car.name.toLowerCase().includes(carBrand.toLowerCase()));
+    const totalCost = filteredCarList.reduce((acc, car) => acc + car.cost, 0);
     return (
-        <div>CarForm</div>
+        <div className="car-value">
+            Total Cost: ${totalCost}
+        </div>
     );
 }
