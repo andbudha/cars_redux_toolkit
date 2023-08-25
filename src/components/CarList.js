288fbe72283a9cfcd48a271621f1696a91dac216
andbudha <andreibartov@gmail.com>
2023-08-25 21:20:09 +0200

    CarList comp updated/name-match highlighting added

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -3,16 +3,18 @@ import { removeCar } from "../store";
 
 export const CarList = () => {
     const dispatch = useDispatch();
+    const carBrand = useSelector(state => state.form.name);
     const cars = useSelector(state => state.cars.carList);
-    const carBrand = useSelector(state => state.cars.searchTerm);
-    const filteredCars = cars.filter(car => car.name.toLowerCase().includes(carBrand.toLowerCase()));
+    const searchedCarBrand = useSelector(state => state.cars.searchTerm);
+    const filteredCars = cars.filter(car => car.name.toLowerCase().includes(searchedCarBrand.toLowerCase()));
 
     const handleCarDelete = (car) => {
         dispatch(removeCar(car.id))
     }
     const renderedCars = filteredCars.map(car => {
+        const bold = carBrand && car.name.toLowerCase().includes(carBrand.toLowerCase())
         return (
-            <div key={car.id} className="panel">
+            <div key={car.id} className={`panel ${bold && 'bold'}`}>
                 <p>{car.name} - ${car.cost}</p>
                 <button
                     className="button is-danger"
@@ -30,3 +32,4 @@ export const CarList = () => {
         </div>
     );
 }
+
