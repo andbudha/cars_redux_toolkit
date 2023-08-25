9715075967e9e2d283344340fd0183177a21bea5
andbudha <andreibartov@gmail.com>
2023-08-25 20:09:44 +0200

    car-list filtering func-ty added

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -4,11 +4,13 @@ import { removeCar } from "../store";
 export const CarList = () => {
     const dispatch = useDispatch();
     const cars = useSelector(state => state.cars.carList);
-    console.log(cars);
+    const carBrand = useSelector(state => state.cars.searchTerm);
+    const filteredCars = cars.filter(car => car.name.toLowerCase().includes(carBrand.toLowerCase()));
+
     const handleCarDelete = (car) => {
         dispatch(removeCar(car.id))
     }
-    const renderedCars = cars.map(car => {
+    const renderedCars = filteredCars.map(car => {
         return (
             <div key={car.id} className="panel">
                 <p>{car.name} - ${car.cost}</p>
